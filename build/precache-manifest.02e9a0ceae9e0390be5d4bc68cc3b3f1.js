57b7c166f61e2588df6cb93adfdfe1a09dcb306b
grmo <43529985+grmo@users.noreply.github.com>
2020-12-18 19:20:33 +0000

    Add project files and info

---
+++ build/precache-manifest.02e9a0ceae9e0390be5d4bc68cc3b3f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b667412df9cc7c6d45097e463b4b34c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "a3bf137c9dfeb7c4196f",
+    "url": "/static/js/main.27489dae.chunk.js"
+  },
+  {
+    "revision": "44105ad7c8f6d07310b3",
+    "url": "/static/js/runtime-main.a19405b8.js"
+  }
+]);
